77a5d6ea9722e6f2d026ac7e8ff45412f6bbcaaf
NGnYan <ngnyan@air-de-ngnyan.home>
2025-03-14 22:55:02 +0100

    update input conditions

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -34,6 +34,12 @@ const firstName = document.querySelector("#first");
 const lastName = document.querySelector("#last");
 const email = document.querySelector("#email");
 const quantity = document.querySelector("#quantity");
+const checkboxInputs = document.querySelectorAll(".checkbox-input");
+const selectedRadio = document.querySelector('input[name="location"]:checked');
+const selectedTerms = document.querySelector("#checkbox1");
+const selectedNews = document.querySelector("#checkbox2");
+
+const inscriptionForm = document.querySelector('input[name="reserve"]');
 
 const isFirstNameCorrect = (firstName) => {
   return firstName.length >= 2 && firstName != "";
@@ -50,4 +56,32 @@ const isEmailCorrect = (email) => {
 
 const isQuantityCorrect = (quantity) => {
   return !isNaN(quantity) && Number(quantity) > 0 && quantity !== "";
+};
+
+const isBtnRadioCorrect = (selectedRadio) => {
+  return selectedRadio !== null;
+};
+
+const isBtnTermsCorrect = (selectedTerms) => {
+  return selectedTerms.checked;
+};
+
+const isBtnNewsCorrect = (selectedNews) => {
+  if (selectedNews) {
+    return true;
+  } else {
+    return true;
+  }
+};
+
+const isFormValid = () => {
+  return (
+    isFirstNameCorrect(firstName) &&
+    isLastNameCorrect(lastName) &&
+    isEmailCorrect(email) &&
+    isQuantityCorrect(quantity) &&
+    isBtnRadioCorrect(selectedRadio) &&
+    isBtnTermsCorrect(selectedTerms) &&
+    isBtnNewsCorrect(selectedNews)
+  );
 };
