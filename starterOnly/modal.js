18eda5cea1369615fd25bc405cb9977a918ee458
NGnYan <ngnyan@air-de-ngnyan.home>
2025-03-16 23:21:32 +0100

    try Form submission

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,34 +13,33 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
 
-// launch modal event
+// Launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// Launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close modal element
+// Close modal element
 closeBtn.addEventListener("click", closeModal);
 
-// close modal form
+// Close the form modal
 function closeModal() {
   modalbg.style.display = "none";
 }
 
-// Validation submit
+// Conditions elements
 const firstName = document.querySelector("#first");
 const lastName = document.querySelector("#last");
 const email = document.querySelector("#email");
 const quantity = document.querySelector("#quantity");
-const checkboxInputs = document.querySelectorAll(".checkbox-input");
-const selectedRadio = document.querySelector('input[name="location"]:checked');
+const selectedRadio = document.querySelectorAll('input[name="location"]');
 const selectedTerms = document.querySelector("#checkbox1");
 const selectedNews = document.querySelector("#checkbox2");
+const inscriptionForm = document.querySelector(".btn-submit");
 
-const inscriptionForm = document.querySelector('input[name="reserve"]');
-
+// Conditions for valid form submission
 const isFirstNameCorrect = (firstName) => {
   return firstName.length >= 2 && firstName != "";
 };
@@ -55,11 +54,11 @@ const isEmailCorrect = (email) => {
 };
 
 const isQuantityCorrect = (quantity) => {
-  return !isNaN(quantity) && Number(quantity) > 0 && quantity !== "";
+  return !isNaN(quantity) && Number(quantity) > 0;
 };
 
 const isBtnRadioCorrect = (selectedRadio) => {
-  return selectedRadio !== null;
+  return selectedRadio.checked;
 };
 
 const isBtnTermsCorrect = (selectedTerms) => {
@@ -74,14 +73,28 @@ const isBtnNewsCorrect = (selectedNews) => {
   }
 };
 
-const isFormValid = () => {
-  return (
-    isFirstNameCorrect(firstName) &&
-    isLastNameCorrect(lastName) &&
-    isEmailCorrect(email) &&
-    isQuantityCorrect(quantity) &&
-    isBtnRadioCorrect(selectedRadio) &&
-    isBtnTermsCorrect(selectedTerms) &&
-    isBtnNewsCorrect(selectedNews)
-  );
-};
+// Form submission
+
+inscriptionForm.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const isFormValid = () => {
+    return (
+      isFirstNameCorrect(firstName) &&
+      isLastNameCorrect(lastName) &&
+      isEmailCorrect(email) &&
+      isQuantityCorrect(quantity) &&
+      isBtnRadioCorrect(selectedRadio) &&
+      isBtnTermsCorrect(selectedTerms) &&
+      isBtnNewsCorrect(selectedNews)
+    );
+  };
+
+  const conditionsValid = isFormValid();
+
+  if (isFormValid()) {
+    console.log("Formulaire valide");
+  } else {
+    console.log("Formulaire invalide");
+  }
+});
